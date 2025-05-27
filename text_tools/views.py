57916c1af95d5553bc673b047204661c233763b4
Maxim <maxsavikin@gmail.com>
2025-05-27 16:15:03 +0300

    Добавил раздел "Шифрование"

--- text_tools/views.py
+++ text_tools/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .func import *
+
+
 # Create your views here.
 class AnalysesView(View):
     def get(self, request):
@@ -9,7 +11,8 @@ class AnalysesView(View):
     def post(self, request):
         string = request.POST.get('text', '')
         result = string_analyses(string)
-        return render(request, 'text_tools/results/result_analyses.html', {"result":result})
+        return render(request, 'text_tools/results/result_analyses.html', {"result": result})
+
 
 class RegisterConvertorView(View):
     def get(self, request):
@@ -21,6 +24,7 @@ class RegisterConvertorView(View):
         result = convert_register(string, choice)
         return render(request, "text_tools/results/result_convertor.html", {"item": result})
 
+
 class StringStripView(View):
     def get(self, request):
         return render(request, "text_tools/string_strip.html")
@@ -29,3 +33,13 @@ class StringStripView(View):
         string = request.POST.get("text", "")
         result = strip_spaces(string)
         return render(request, "text_tools/results/result_strip.html", {"item": result})
+
+
+class StringHashView(View):
+    def get(self, request):
+        return render(request, "text_tools/string_hash.html")
+
+    def post(self, request):
+        string = request.POST.get("text", "")
+        result = hash_string(string)
+        return render(request, "text_tools/results/result_hash.html", {"item": result})
