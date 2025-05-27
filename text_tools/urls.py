59f888c03007bf58237bc507c6245bad1ed055a3
Maxim <maxsavikin@gmail.com>
2025-05-27 15:54:47 +0300

    Добавил раздел "Удаление пробелов"

--- text_tools/urls.py
+++ text_tools/urls.py
@@ -6,5 +6,6 @@ app_name = "text_tools"
 urlpatterns = [
     path("analyses/", views.AnalysesView.as_view(), name='analyses'),
     path('register_convertor/', views.RegisterConvertorView.as_view(), name='register_convertor'),
+    path('string_strip/', views.StringStripView.as_view(), name='string_strip')
 
 ]
